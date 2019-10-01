40d86f56378a5204e98140461f94c8c4c8fa0dee
Aitor Gomez Llavori <agomez257@ikasle.ehu.eus>
2019-10-01 15:27:34 +0200

    Combobox-a eginda

--- src/main/java/ehu/isad/ComboBoxExperiments.java
+++ src/main/java/ehu/isad/ComboBoxExperiments.java
@@ -2,10 +2,17 @@ package ehu.isad;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
 
@@ -19,19 +26,73 @@ public class ComboBoxExperiments extends Application  {
 
         ComboBox comboBox = new ComboBox();
 
-        comboBox.getItems().add("Choice 1");
-        comboBox.getItems().add("Choice 2");
-        comboBox.getItems().add("Choice 3");
+        comboBox.getItems().add("Bidaiak");
+        comboBox.getItems().add("Herri kirolak");
+        comboBox.getItems().add("Saskibaloia");
 
-        comboBox.setEditable(true);
+        comboBox.setEditable(false);
+
+        ListView listView = new ListView();
+
+        ImageView imageView1 = new ImageView();
 
         comboBox.setOnAction(e -> {
-            System.out.println( comboBox.getValue());
+
+            listView.getItems().clear();
+
+
+            String bilduma = (String) comboBox.getValue();
+            if (bilduma=="Bidaiak") {
+                listView.getItems().add("1.1");
+                listView.getItems().add("1.2");
+                listView.getItems().add("1.3");
+                try {
+                    FileInputStream input = new FileInputStream("/home/aitor/Escritorio/ISAD/argazkia.jpg");
+                    Image image = new Image(input);
+                    imageView1.setImage(image);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+            if (bilduma=="Herri kirolak") {
+                listView.getItems().add("2.1");
+                listView.getItems().add("2.2");
+                listView.getItems().add("2.3");
+
+                try {
+                    FileInputStream input = new FileInputStream("/home/aitor/Escritorio/ISAD/Alonsotegi Herri Kirolak 001.jpg");
+                    Image image = new Image(input);
+                    imageView1.setImage(image);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+            if (bilduma=="Saskibaloia") {
+                listView.getItems().add("3.1");
+                listView.getItems().add("3.2");
+                listView.getItems().add("3.3");
+                try {
+                    FileInputStream input = new FileInputStream("/home/aitor/Escritorio/ISAD/Doncic.jpg");
+                    Image image = new Image(input);
+                    imageView1.setImage(image);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+
+
+
+
+
         });
 
-        HBox hbox = new HBox(comboBox);
-
-        Scene scene = new Scene(hbox, 200, 120);
+        imageView1.setFitHeight(150);
+        imageView1.setFitWidth(150);
+        HBox hbox = new HBox(comboBox,listView,imageView1);
+        Scene scene = new Scene(hbox, 400, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
 
